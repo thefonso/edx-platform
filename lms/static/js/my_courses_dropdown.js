4dc2b6e1eed96fc02b04fea381623ccc399e4fb1
Giulio Gratta <giulio@giuliogratta.com>
2013-11-01 13:16:16 -0700

    User menu a11y changes to allow use of spacebar, escape key and arrow keys or tab and shift+tab to navigate

--- lms/static/js/my_courses_dropdown.js
+++ lms/static/js/my_courses_dropdown.js
@@ -1,9 +1,73 @@
 $(document).ready(function () {
-  $('a.dropdown').toggle(function() {
-    $('ul.dropdown-menu').addClass("expanded");
-    $('a.dropdown').addClass("active");
+  // define variables for code legibility
+  var dropdownMenuToggle = $('a.dropdown');
+  var dropdownMenu = $('ul.dropdown-menu');
+  var menuItems = dropdownMenu.find('a');
+  
+  // bind menu toggle click for later use
+  dropdownMenuToggle.toggle(function() {
+    dropdownMenu.addClass("expanded").find('a').first().focus();
+    dropdownMenuToggle.addClass("active").attr("aria-expanded", "true");
   }, function() {
-    $('ul.dropdown-menu').removeClass("expanded");
-    $('a.dropdown').removeClass("active");
+    dropdownMenu.removeClass("expanded");
+    dropdownMenuToggle.removeClass("active").attr("aria-expanded", "false").focus();
   });
+  
+  //catch keypresses when focused on dropdownMenuToggle and dropdownMenu
+  dropdownMenuToggle.on('keydown', function(event){
+    catchKeyPress($(this), event);
+  });
+  dropdownMenu.on('keydown', function(event){
+    catchKeyPress($(this), event);
+  });
+  
+  function catchKeyPress(object, event) {
+    // get currently focused item
+    var focusedItem = jQuery(':focus');
+
+    // get the number of focusable items
+    var numberOfMenuItems = menuItems.length
+
+    // get the index of the currently focused item
+    var focusedItemIndex = menuItems.index(focusedItem);
+    
+    // var to store next focused item index
+    var itemToFocusIndex;
+    
+    // if space key pressed
+	if ( event.which == 32) {
+      dropdownMenuToggle.click();
+      event.preventDefault();
+    }
+    
+    // if escape key pressed and menu open
+	if ( event.which == 27 && (object.hasClass('expanded') || object.hasClass('active'))) {
+      dropdownMenuToggle.click();
+      event.preventDefault();
+    }
+    
+    // if up arrow key pressed or shift+tab
+    if ((event.which == 38 || (event.which == 9 && event.shiftKey)) && (object.hasClass('expanded') || object.hasClass('active'))) {
+      // if first item go to last
+      if (focusedItemIndex === 0) {
+        menuItems.last().focus();
+      } else {
+        itemToFocusIndex = focusedItemIndex - 1;
+        menuItems.get(itemToFocusIndex).focus();
+      }
+      event.preventDefault();
+    }
+    
+    // if down arrow key pressed or tab key
+    if ((event.which == 40 || event.which == 9) && (object.hasClass('expanded') || object.hasClass('active'))) {
+      // if last item go to first
+      if (focusedItemIndex == numberOfMenuItems - 1) {
+        menuItems.first().focus();
+      } else {
+        itemToFocusIndex = focusedItemIndex + 1;
+        menuItems.get(itemToFocusIndex).focus();
+      }
+      event.preventDefault();
+    }
+  }
 });
